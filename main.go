bc42030a1979f02f5a036fad58b421d19723dd41
radii5 <Larkyndel@proton.me>
2026-03-16 15:23:34 -0400

    fix: disable QuickEdit mode on Windows

--- main.go
+++ main.go
@@ -3,21 +3,56 @@ package main
 import (
 	"fmt"
 	"os"
-
+	"runtime"
+	"syscall"
+	"unsafe"
+	
 	"github.com/fatih/color"
 	"github.com/radii5/radii5/cmd"
 )
 
 var version = "0.1.0"
 
+// Windows QuickEdit mode constants
+const (
+	enableQuickEditMode = 0x0040
+	enableExtendedFlags = 0x0080
+)
+
+// disableQuickEdit prevents Windows terminal from pausing on click
+func disableQuickEdit() {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	getStdHandle := kernel32.NewProc("GetStdHandle")
+	getConsoleMode := kernel32.NewProc("GetConsoleMode")
+	setConsoleMode := kernel32.NewProc("SetConsoleMode")
+
+	// STD_INPUT_HANDLE = -10
+	handle, _, _ := getStdHandle.Call(uintptr(^uint(10) + 1))
+
+	var mode uint32
+	getConsoleMode.Call(handle, uintptr(unsafe.Pointer(&mode)))
+
+	// Disable QuickEdit, enable extended flags
+	mode &^= enableQuickEditMode
+	mode |= enableExtendedFlags
+
+	setConsoleMode.Call(handle, uintptr(mode))
+}
+
 func main() {
+	// Disable QuickEdit mode on Windows to prevent accidental pausing
+	disableQuickEdit()
+
 	if len(os.Args) < 2 {
 		printUsage()
 		os.Exit(0)
 	}
 
 	arg := os.Args[1]
-
 	switch arg {
 	case "--version", "-v":
 		fmt.Printf("radii5 v%s\n", version)
